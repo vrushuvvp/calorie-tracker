502b4c4d6c38f162048f0ec74beb3865e9f024af
vrushuvvp <vrushank07vvp@gmail.com>
2025-04-06 14:25:25 +0530

    Updates

---
+++ precache-manifest.dacee59d1a8116b8b96f5355abc2d7f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa437033f78e8e162a5be55ba204507",
+    "url": "/calorie-tracker/index.html"
+  },
+  {
+    "revision": "40b0b7f43a3ea07312a5",
+    "url": "/calorie-tracker/static/css/main.1d59a27c.chunk.css"
+  },
+  {
+    "revision": "0c7b96c9ac02df515fe5",
+    "url": "/calorie-tracker/static/js/2.48f12f5d.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/calorie-tracker/static/js/2.48f12f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913f26e3ed52c195f8ca",
+    "url": "/calorie-tracker/static/js/3.ddc6a9a6.chunk.js"
+  },
+  {
+    "revision": "40b0b7f43a3ea07312a5",
+    "url": "/calorie-tracker/static/js/main.5799f31a.chunk.js"
+  },
+  {
+    "revision": "8ece6a56a6e88916a4c7",
+    "url": "/calorie-tracker/static/js/runtime-main.1acd7218.js"
+  }
+]);
